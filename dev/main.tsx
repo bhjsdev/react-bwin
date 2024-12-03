50a7a207cf6e61bb72af061db9a2c414f861bfcf
Bhjs Dev <bhjsdev@gmail.com>
2024-12-03 19:19:11 +1100

    fix: downgrade React and ReactDOM to support lower versions

--- dev/main.tsx
+++ dev/main.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { createRoot } from 'react-dom/client'
+import ReactDOM from 'react-dom'
 import Window from '../src/Window'
 import Counter from './Counter'
 
@@ -39,4 +39,4 @@ function App() {
 	)
 }
 
-createRoot(document.getElementById('root')!).render(<App />)
+ReactDOM.render(<App />, document.getElementById('root'))
