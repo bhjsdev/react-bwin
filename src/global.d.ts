a2faf6937703fe7f46078f4f153d7a30b5fd42c6
Bhjs Dev <bhjsdev@gmail.com>
2024-12-04 09:49:39 +1100

    fix: types exported

--- src/global.d.ts
+++ src/global.d.ts
@@ -3,90 +3,85 @@ declare module 'react-bwin' {
 	export const Window: React.FC<WindowProps>
 }
 
-declare module 'bwin' {
-	export const BUILTIN_ACTIONS: Action[]
-	export const BinaryWindow: BinaryWindow
-}
+declare global {
+	type Position = 'left' | 'right' | 'top' | 'bottom'
 
-declare namespace JSX {
-	interface IntrinsicElements {
-		'bw-window': React.DetailedHTMLProps<any, HTMLElement>
-		'bw-pane': React.DetailedHTMLProps<any, HTMLElement>
-		'bw-muntin': React.DetailedHTMLProps<any, HTMLElement>
-		'bw-glass': React.DetailedHTMLProps<any, HTMLElement>
-		'bw-glass-header': React.DetailedHTMLProps<any, HTMLElement>
-		'bw-glass-title': React.DetailedHTMLProps<any, HTMLElement>
-		'bw-glass-action-container': React.DetailedHTMLProps<any, HTMLElement>
-		'bw-glass-content': React.DetailedHTMLProps<any, HTMLElement>
+	type Action = {
+		label?: string
+		className?: string
+		onClick: (
+			event: React.MouseEvent<HTMLButtonElement>,
+			bwin: BinaryWindow
+		) => void
 	}
-}
 
-type Position = 'left' | 'right' | 'top' | 'bottom'
+	type Actions = undefined | null | Action[]
 
-type Action = {
-	label?: string
-	className?: string
-	onClick: (
-		event: React.MouseEvent<HTMLButtonElement>,
-		bwin: BinaryWindow
-	) => void
-}
+	interface Sash {
+		id: string
+		position: Position
+		left: number
+		top: number
+		width: number
+		height: number
+		children: Sash[]
+		domNode?: HTMLElement
+		leftChild?: Sash
+		rightChild?: Sash
+		topChild?: Sash
+		bottomChild?: Sash
+		store?: {
+			actions?: Actions
+			droppable?: boolean
+			draggable?: boolean
+			title?: any
+			content?: any
+			resizable?: boolean
+		}
+		walk(callback: (sash: Sash) => void): void
+	}
 
-type Actions = undefined | null | Action[]
-
-interface Sash {
-	id: string
-	position: Position
-	left: number
-	top: number
-	width: number
-	height: number
-	children: Sash[]
-	domNode?: HTMLElement
-	leftChild?: Sash
-	rightChild?: Sash
-	topChild?: Sash
-	bottomChild?: Sash
-	store?: {
+	type ConfigNode = {
+		id?: string
+		size?: number
+		position?: Position
+		title?: React.ReactNode
+		content?: React.ReactNode
 		actions?: Actions
-		droppable?: boolean
+		children?: ConfigNode[]
 		draggable?: boolean
-		title?: any
-		content?: any
-		resizable?: boolean
+		droppable?: boolean
 	}
-	walk(callback: (sash: Sash) => void): void
+
+	type ConfigRoot = {
+		id?: string
+		width?: number
+		height?: number
+		fitContainer?: boolean
+		title?: React.ReactNode
+		content?: React.ReactNode
+		children?: ConfigNode[]
+	}
+
+	interface BinaryWindow {
+		new (settings: ConfigRoot): BinaryWindow
+		rootSash: Sash
+		windowElement: HTMLElement
+		containerElement: HTMLElement
+		mount(container: HTMLElement): void
+		enableFeatures(): void
+	}
+
+	type WindowProps = ConfigRoot
 }
 
-type ConfigNode = {
-	id?: string
-	size?: number
-	position?: Position
-	title?: React.ReactNode
-	content?: React.ReactNode
-	actions?: Actions
-	children?: ConfigNode[]
-	draggable?: boolean
-	droppable?: boolean
+export {
+	Position,
+	Action,
+	Actions,
+	Sash,
+	ConfigNode,
+	ConfigRoot,
+	BinaryWindow,
+	WindowProps,
 }
-
-type ConfigRoot = {
-	id?: string
-	width?: number
-	height?: number
-	fitContainer?: boolean
-	title?: React.ReactNode
-	content?: React.ReactNode
-	children?: ConfigNode[]
-}
-
-interface BinaryWindow {
-	new (settings: ConfigRoot): BinaryWindow
-	rootSash: Sash
-	windowElement: HTMLElement
-	containerElement: HTMLElement
-	mount(container: HTMLElement): void
-	enableFeatures(): void
-}
-
-type WindowProps = ConfigRoot
