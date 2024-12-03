b0539dd8b26607b409be703b8b9ddc73c131027b
Bhjs Dev <bhjsdev@gmail.com>
2024-12-03 14:43:31 +1100

    build: update config

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,21 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  plugins: [react()],
-  root: "./dev",
-  envDir: "../",
-  build: {
-    outDir: "../dist",
-    emptyOutDir: true,
-    lib: {
-      entry: "../src/Window.tsx",
-      formats: ["es"],
-      name: "ReactBinaryWindow",
-      fileName: "react-bwin",
-    },
-  },
-});
+	plugins: [react()],
+	root: './dev',
+	envDir: '../',
+	build: {
+		outDir: '../dist',
+		emptyOutDir: true,
+		lib: {
+			entry: '../src/index.tsx',
+			formats: ['es'],
+			name: 'ReactBinaryWindow',
+			fileName: 'react-bwin',
+		},
+		rollupOptions: {
+			external: ['react', 'react-dom', 'react/jsx-runtime', 'bwin'],
+		},
+	},
+})
