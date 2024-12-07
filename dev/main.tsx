466b6509245f250afb1e001fc93799280bd45c38
Bhjs Dev <bhjsdev@gmail.com>
2024-12-08 09:56:03 +1100

    test: use useMemo api for keep Window not changes when parent updates

--- dev/main.tsx
+++ dev/main.tsx
@@ -1,59 +1,70 @@
-import React from 'react'
+import React, { useState, memo, useMemo } from 'react'
 import ReactDOM from 'react-dom'
 import { Window, BUILTIN_ACTIONS } from '../src'
 import Counter from './Counter'
 
 function App() {
+	const [count, setCount] = useState(0)
+
+	const windowNode = (
+		<Window
+			id="root"
+			width={444}
+			height={333}
+			fitContainer={false}
+			content={<em>Root!</em>}
+			panes={[
+				{
+					size: 0.4,
+					content: 'hello world',
+					actions: [
+						{
+							label: 'Update',
+							onClick: (event, bwin) => {
+								const glassEl = (event.target as HTMLButtonElement).closest(
+									'bw-glass'
+								) as HTMLElement
+								const contentEl = glassEl.querySelector('bw-glass-content')
+
+								if (contentEl) {
+									contentEl.innerHTML = `Bye world ${bwin.rootSash.id}`
+								}
+							},
+						},
+						...BUILTIN_ACTIONS,
+					],
+				},
+				{
+					children: [
+						{
+							id: 'top-right',
+							size: 0.5,
+							position: 'top',
+							title: 'bye world',
+							content: <Counter />,
+						},
+						{
+							id: 'bottom-right',
+							title: <mark>no drag</mark>,
+							draggable: false,
+						},
+					],
+				},
+			]}
+		/>
+	)
+
+	const memoizedNode = useMemo(() => windowNode, [])
+
 	return (
 		<div
 			id="react-container"
 			style={{ width: 400, height: 400, backgroundColor: 'pink' }}
 		>
-			<Window
-				id="root"
-				width={444}
-				height={333}
-				fitContainer={false}
-				content={<em>Root!</em>}
-				panes={[
-					{
-						size: 0.4,
-						content: 'hello world',
-						actions: [
-							{
-								label: 'Update',
-								onClick: (event, bwin) => {
-									const glassEl = (event.target as HTMLButtonElement).closest(
-										'bw-glass'
-									) as HTMLElement
-									const contentEl = glassEl.querySelector('bw-glass-content')
-
-									if (contentEl) {
-										contentEl.innerHTML = `Bye world ${bwin.rootSash.id}`
-									}
-								},
-							},
-							...BUILTIN_ACTIONS,
-						],
-					},
-					{
-						children: [
-							{
-								id: 'top-right',
-								size: 0.5,
-								position: 'top',
-								title: 'bye world',
-								content: <Counter />,
-							},
-							{
-								id: 'bottom-right',
-								title: <mark>no drag</mark>,
-								draggable: false,
-							},
-						],
-					},
-				]}
-			/>
+			<button onClick={() => setCount((count) => count + 1)}>
+				count is {count}
+			</button>
+			{memoizedNode}
 		</div>
 	)
 }
