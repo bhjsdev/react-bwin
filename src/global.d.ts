0e24ef00f443eb29a48c0bc3213e4d4ce842a9c9
Bhjs Dev <bhjsdev@gmail.com>
2024-12-12 10:48:02 +1100

    fix: size can be string

--- src/global.d.ts
+++ src/global.d.ts
@@ -1,97 +1,97 @@
 declare module 'react-bwin' {
-	export const BUILTIN_ACTIONS: Action[]
-	export const Window: React.FC<WindowProps>
+  export const BUILTIN_ACTIONS: Action[]
+  export const Window: React.FC<WindowProps>
 }
 
 declare global {
-	type Position = 'left' | 'right' | 'top' | 'bottom'
+  type Position = 'left' | 'right' | 'top' | 'bottom'
 
-	type Action = {
-		label?: string
-		className?: string
-		onClick: (
-			event: React.MouseEvent<HTMLButtonElement>,
-			bwin: BinaryWindow
-		) => void
-	}
+  type Action = {
+    label?: string
+    className?: string
+    onClick: (
+      event: React.MouseEvent<HTMLButtonElement>,
+      bwin: BinaryWindow
+    ) => void
+  }
 
-	type Actions = undefined | null | Action[]
+  type Actions = undefined | null | Action[]
 
-	interface Sash {
-		id: string
-		position: Position
-		left: number
-		top: number
-		width: number
-		height: number
-		minWidth: number
-		minHeight: number
-		children: Sash[]
-		domNode?: HTMLElement
-		leftChild?: Sash
-		rightChild?: Sash
-		topChild?: Sash
-		bottomChild?: Sash
-		store?: {
-			actions?: Actions
-			droppable?: boolean
-			draggable?: boolean
-			title?: any
-			content?: any
-			resizable?: boolean
-		}
-		walk(callback: (sash: Sash) => void): void
-	}
+  interface Sash {
+    id: string
+    position: Position
+    left: number
+    top: number
+    width: number
+    height: number
+    minWidth: number
+    minHeight: number
+    children: Sash[]
+    domNode?: HTMLElement
+    leftChild?: Sash
+    rightChild?: Sash
+    topChild?: Sash
+    bottomChild?: Sash
+    store?: {
+      actions?: Actions
+      droppable?: boolean
+      draggable?: boolean
+      title?: any
+      content?: any
+      resizable?: boolean
+    }
+    walk(callback: (sash: Sash) => void): void
+  }
 
-	type ConfigNode = {
-		id?: string
-		size?: number
-		position?: Position
-		title?: React.ReactNode
-		content?: React.ReactNode
-		actions?: Actions
-		children?: ConfigNode[]
-		draggable?: boolean
-		droppable?: boolean
-	}
+  type ConfigNode = {
+    id?: string
+    size?: number | string
+    position?: Position
+    title?: React.ReactNode
+    content?: React.ReactNode
+    actions?: Actions
+    children?: ConfigNode[]
+    draggable?: boolean
+    droppable?: boolean
+  }
 
-	type ConfigRoot = {
-		id?: string
-		width?: number
-		height?: number
-		fitContainer?: boolean
-		title?: React.ReactNode
-		content?: React.ReactNode
-		children?: ConfigNode[]
-	}
+  type ConfigRoot = {
+    id?: string
+    width?: number
+    height?: number
+    fitContainer?: boolean
+    title?: React.ReactNode
+    content?: React.ReactNode
+    children?: ConfigNode[]
+  }
 
-	interface BinaryWindow {
-		new (settings: ConfigRoot): BinaryWindow
-		rootSash: Sash
-		windowElement: HTMLElement
-		containerElement: HTMLElement
-		sillElement: HTMLElement
-		mount(container: HTMLElement): void
-		enableFeatures(): void
-		fit(): void
-	}
+  interface BinaryWindow {
+    new (settings: ConfigRoot): BinaryWindow
+    rootSash: Sash
+    windowElement: HTMLElement
+    containerElement: HTMLElement
+    sillElement: HTMLElement
+    mount(container: HTMLElement): void
+    enableFeatures(): void
+    fit(): void
+  }
 
-	type WindowRef = {
-		binaryWindow: BinaryWindow
-	}
+  type WindowRef = {
+    binaryWindow: BinaryWindow
+  }
 
-	type WindowProps = Omit<ConfigRoot, 'children'> & {
-		panes?: ConfigNode[]
-	}
+  type WindowProps = Omit<ConfigRoot, 'children'> & {
+    panes?: ConfigNode[]
+  }
 }
 
 export {
-	Position,
-	Action,
-	Actions,
-	Sash,
-	ConfigNode,
-	ConfigRoot,
-	BinaryWindow,
-	WindowProps,
+  Position,
+  Action,
+  Actions,
+  Sash,
+  ConfigNode,
+  ConfigRoot,
+  BinaryWindow,
+  WindowProps,
 }
