ced7dc60d4d5a84232bf4d2e97b58f6d3519f417
Bhjs Dev <bhjsdev@gmail.com>
2024-12-14 09:32:18 +1100

    chore: removed memoized code in dev

--- dev/adhoc.tsx
+++ dev/adhoc.tsx
@@ -1,9 +1,28 @@
 import React from 'react'
+import { Window } from '../src'
 
 export default function App() {
   return (
     <div style={{ padding: 20 }}>
-      <h1>Adhoc</h1>
+      <h2>Adhoc</h2>
+      <Window
+        id="root"
+        width={444}
+        height={333}
+        panes={[
+          {
+            size: 0.4,
+          },
+          {
+            children: [
+              {
+                size: 0.5,
+                position: 'top',
+              },
+            ],
+          },
+        ]}
+      />
     </div>
   )
 }
--- dev/fit-container.tsx
+++ dev/fit-container.tsx
@@ -4,41 +4,35 @@ import { Window } from '../src'
 export default function App() {
   const windowRef = useRef<WindowRef>(null)
 
-  const windowNode = (
-    <Window
-      ref={windowRef}
-      id="root"
-      width={444}
-      height={333}
-      fitContainer={false}
-      content={<em>Root!</em>}
-      panes={[
-        {
-          size: 0.4,
-        },
-        {
-          children: [
-            {
-              id: 'top-right',
-              size: 0.5,
-              position: 'top',
-            },
-            {
-              id: 'bottom-right',
-            },
-          ],
-        },
-      ]}
-    />
-  )
-
-  const memoizedWindowNode = useMemo(() => windowNode, [])
-
   return (
     <div style={{ padding: 20 }}>
       <button onClick={() => windowRef.current?.binaryWindow.fit()}>Fit</button>
       <div style={{ width: 800, height: 400, backgroundColor: 'pink' }}>
-        {memoizedWindowNode}
+        <Window
+          ref={windowRef}
+          id="root"
+          width={444}
+          height={333}
+          fitContainer={false}
+          content={<em>Root!</em>}
+          panes={[
+            {
+              size: 0.4,
+            },
+            {
+              children: [
+                {
+                  id: 'top-right',
+                  size: 0.5,
+                  position: 'top',
+                },
+                {
+                  id: 'bottom-right',
+                },
+              ],
+            },
+          ]}
+        />
       </div>
     </div>
   )
