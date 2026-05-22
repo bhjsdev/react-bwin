25c60378d3eeeeba096e879838ab59f1f51013f2
Oh Xyz <ohxyzcom@gmail.com>
2026-05-22 12:23:21 +1000

    example: fix minor visual issues

--- examples/ide/src/App.tsx
+++ examples/ide/src/App.tsx
@@ -113,7 +113,7 @@ export default function App() {
             {
               id: 'sidebar',
               position: 'left',
-              size: '220px',
+              size: 0.2,
               title: 'Explorer',
               content: <FileExplorer />,
               actions: null,
